83f2db07649217286280f72c89f2ea58d86a65bb
AnneyshaC <46805629+AnneyshaC@users.noreply.github.com>
2019-07-31 17:22:43 +0530

    Add files via upload

---
+++ Assessments/day3/hello.js
@@ -0,0 +1,18 @@
+function sayHello() {
+ alert("Hello World, this is from js file");
+}
+function color(x){
+	x.style.background="yellow";
+}
+function col(y){
+	y.style.background="pink";
+}
+
+function add(a, b)
+{
+var c;
+c = a + b;
+console.log('Can u see this?');
+alert('The result is '+c);
+
+}
